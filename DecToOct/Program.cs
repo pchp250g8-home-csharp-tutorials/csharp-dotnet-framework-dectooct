cfcb7f9e0eb9240cb415e35e9336e302602c772f
Alexander <pchp250g8@tutamail.com>
2026-04-09 12:32:37 +0300

    Add project files.

---
+++ DecToOct/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecToOct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const uint MAX_INT = uint.MaxValue;
+            Console.WriteLine("Input an unsigned integer number");
+            var bIsRightNumber = (uint.TryParse(Console.ReadLine(), out var uNumber)) &&
+                                 (uNumber <= MAX_INT);
+            if (!bIsRightNumber)
+            {
+                Console.WriteLine("Invalid number format or number too big");
+                Console.Read();
+                return;
+            }
+            var strOctNum = "";
+            var uTempVal = uNumber;
+            while (uTempVal > 0)
+            {
+                strOctNum = (uTempVal % 8) + strOctNum;
+                uTempVal /= 8;
+            }
+            if (strOctNum.Length == 0)
+                strOctNum = "0";
+            Console.WriteLine
+            (
+                "The octal equivalent of the decimal number {0} is: {1}",
+                uNumber, strOctNum
+            );
+            Console.Read();
+        }
+    }
+}
